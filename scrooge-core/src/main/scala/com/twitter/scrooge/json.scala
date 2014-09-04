77133be3fc6fd6c1adbedb6469b313940b40f7f0
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2014-09-04 11:45:47 -0700

    Clarify types on Type maps

--- scrooge-core/src/main/scala/com/twitter/scrooge/json.scala
+++ scrooge-core/src/main/scala/com/twitter/scrooge/json.scala
@@ -2,6 +2,6 @@ package com.twitter.scrooge
 
 import com.persist.json.{WriteCodec, ReadCodec}
 
-case class Info(in: TInfo, out: TInfo)
+case class Info[T <: ThriftStruct](in: TInfo[T], out: TInfo[T])
 
-case class TInfo(is: ThriftStruct => Boolean, companion: ThriftStructCodec[_ <: ThriftStruct], readCodec: ReadCodec[_], writeCodec: WriteCodec[_])
+case class TInfo[T <: ThriftStruct](is: T => Boolean, companion: ThriftStructCodec[T], readCodec: ReadCodec[T], writeCodec: WriteCodec[T])
