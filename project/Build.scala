15cef847568798f70939b8c02ed1cddcfc49f97b
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2014-09-17 10:06:14 -0700

    Bump version

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 import net.virtualvoid.sbt.cross.CrossPlugin
 
 object Scrooge extends Build {
-  val libVersion = "3.16.38"
+  val libVersion = "3.16.39"
   val utilVersion = "6.20.0"
   val finagleVersion = "6.20.0"
 
